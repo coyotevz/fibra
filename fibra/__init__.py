1deb72482f52ec5801aa799496e5ee9702863327
Augusto Roccasalva <augusto@rioplomo.com.ar>
2017-05-24 16:07:17 -0300

    Add ProxyFix middleware

--- fibra/__init__.py
+++ fibra/__init__.py
@@ -13,7 +13,10 @@ from fibra.models import db
 import fibra.scssfilter # webasset filter
 from fibra.jinjafilters import dateformat_filter, timeago_filter, moneyfmt_filter
 
+from werkzeug.contrib.fixers import ProxyFix
+
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 # Config
 try:
